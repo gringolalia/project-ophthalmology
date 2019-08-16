b2dd8ac8492cf4850a01c7f353d0cd397ba77d2d
Colin Brayton <gringolalia@gmail.com>
2019-08-16 15:12:13 -0300

    commission of original sin

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './registerServiceWorker'
+import Vuikit from 'vuikit'
+import VuikitIcons from '@vuikit/icons'
+
+import '@vuikit/theme'
+
+Vue.use(Vuikit)
+Vue.use(VuikitIcons)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
