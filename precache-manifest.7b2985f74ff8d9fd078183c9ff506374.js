362c1286741f8a0b0535854ca92fe6dd4f1a5d03
Colin Brayton <gringolalia@gmail.com>
2020-02-10 15:20:52 -0300

    deploy

---
+++ precache-manifest.7b2985f74ff8d9fd078183c9ff506374.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a01762d3e8731575246",
+    "url": "/project-ophthalmology/js/about.4c2d042f.js"
+  },
+  {
+    "revision": "b918121b364bf0677913",
+    "url": "/project-ophthalmology/css/app.08aa0a38.css"
+  },
+  {
+    "revision": "b918121b364bf0677913",
+    "url": "/project-ophthalmology/js/app.53e5feb3.js"
+  },
+  {
+    "revision": "6842f763328a690827a9",
+    "url": "/project-ophthalmology/css/chunk-vendors.d642b461.css"
+  },
+  {
+    "revision": "6842f763328a690827a9",
+    "url": "/project-ophthalmology/js/chunk-vendors.20b73163.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/project-ophthalmology/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "721e015064b9fd26a957d53c4f6943b8",
+    "url": "/project-ophthalmology/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/project-ophthalmology/robots.txt"
+  },
+  {
+    "revision": "63cb184b6518339985b6bbf00929b73c",
+    "url": "/project-ophthalmology/vuikit-icons.js"
+  },
+  {
+    "revision": "316c44c1887fc69bb4b6cde7b55ebff6",
+    "url": "/project-ophthalmology/vuikit-icons.min.js"
+  },
+  {
+    "revision": "6e874f62583874671c2cd23e5c178cb8",
+    "url": "/project-ophthalmology/vuikit.js"
+  },
+  {
+    "revision": "b59fda7e823d96b215b4f2aaa15f5832",
+    "url": "/project-ophthalmology/vuikit.min.css"
+  },
+  {
+    "revision": "a91c95b3e036d1a603e5e2757a556bee",
+    "url": "/project-ophthalmology/vuikit.min.js"
+  },
+  {
+    "revision": "3217758f0db3b967b92501204a28d78e",
+    "url": "/project-ophthalmology/vuikit.css"
+  }
+];
